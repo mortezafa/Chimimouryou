ffbaf2e39ba125db5085b66fe1c1e4738d1852ec
MortezaFa <teezy10299@gmail.com>
2024-12-10 11:22:15 -0700

    Got a really janky workflow going. Need to refactor though...

--- cmd/search.go
+++ cmd/search.go
@@ -4,11 +4,12 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"net/url"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -24,7 +25,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("search called")
+		fmt.Println("searching... One moment please.")
 		parseJsonData()
 	},
 }
@@ -43,13 +44,53 @@ type AnimeInfo struct {
 	totalEpisodes int32  `json:"totalEpisodes"`
 }
 
-type AnimeEpisodesContent struct {
-	ID     string `json:"id"`
-	number int32  `json:"number"`
-	url    string `json:"url"`
+type AnimeStreams struct {
+	Sources []struct {
+		Url     string `json:"url"`
+		IsM3U8  bool   `json:"isM3U8"`
+		Quality string `json:"quality"`
+	} `json:"sources"`
 }
 
 func parseJsonData() {
+	jsonBody, err := fetchJsonBody()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var animeStreams AnimeStreams
+	err = json.Unmarshal(jsonBody, &animeStreams)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	urls := []string{}
+
+	for _, source := range animeStreams.Sources {
+		urls = append(urls, source.Url)
+	}
+
+	fmt.Println(urls[3])
+
+	playVid := exec.Command("vlc", urls[3])
+
+	err = playVid.Start()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = playVid.Wait()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("COMMAND DONE YAY")
+
+}
+
+func fetchJsonBody() ([]byte, error) {
 	baseURL := "http://localhost:3000/anime/gogoanime/watch/tokyo-ghoul-episode-1"
 
 	params := url.Values{}
@@ -59,17 +100,17 @@ func parseJsonData() {
 
 	resp, err := http.Get(fullURL)
 	if err != nil {
-		log.Fatalf("Failed to make the request: %v", err)
+		fmt.Errorf("Failed to make the request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Request failed with status: %s", resp.Status)
+		fmt.Errorf("Request failed with status: %s", resp.Status)
 	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalf("Failed to read the response body: %v", err)
+		fmt.Errorf("Failed to read the response body: %v", err)
 	}
-	fmt.Println(string(body))
+	return body, nil
 }
